ba702c09df955efd76cbf5efd7c2af042743f06b
Izzet Güntürk <102749769+IzzetGunturk@users.noreply.github.com>
2023-03-03 22:27:25 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php 
-    $servername = "localhost";
-    $username = "root";
+    $servername = "";
+    $username = "";
     $password = "";
-    $dbname = "loginregister";
+    $dbname = "";
     
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
